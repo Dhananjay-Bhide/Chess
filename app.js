eec7c06989acc6c1bb15be90587c06efcc18697d
DhananjayBhide <112177814+Dhananjay-Bhide@users.noreply.github.com>
2024-06-12 17:48:04 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const socket = require('socket.io');
+const http = require('http');
+const { Chess } = require('chess.js');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = socket(server);
+
+const chess = new Chess();
+
+let players = {};
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.render("index", { title: "Chess Game" });
+});
+
+io.on("connection", function (uniqueSocket) {
+    console.log("connected");
+
+    if (!players.white) {
+        players.white = uniqueSocket.id;
+        uniqueSocket.emit("playerRole", "w");
+    } else if (!players.black) {
+        players.black = uniqueSocket.id;
+        uniqueSocket.emit("playerRole", "b");
+    } else {
+        uniqueSocket.emit("spectatorRole");
+    }
+
+    uniqueSocket.emit("boardState", chess.fen()); // Send initial board state
+
+    uniqueSocket.on("disconnect", function () {
+        if (uniqueSocket.id === players.white) {
+            delete players.white;
+        } else if (uniqueSocket.id === players.black) {
+            delete players.black;
+        }
+    });
+
+    uniqueSocket.on("move", (move) => {
+        try {
+            if (chess.turn() === 'w' && uniqueSocket.id !== players.white) return;
+            if (chess.turn() === 'b' && uniqueSocket.id !== players.black) return;
+
+            const result = chess.move(move);
+            if (result) {
+                io.emit("move", move);
+                io.emit("boardState", chess.fen());
+            } else {
+                console.log("Invalid move: ", move);
+                uniqueSocket.emit("message", "Invalid move");
+            }
+        } catch (err) {
+            console.log(err);
+            uniqueSocket.emit("message", "Invalid move");
+        }
+    });
+});
+
+server.listen(3000, function () {
+    console.log("Server running on port 3000");
+});
